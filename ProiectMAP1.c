9a6d08b133b5544f9627324db11013baa5b8aca9
SkkyDMG <lascuteodor15@gmail.com>
2023-01-11 20:23:50 +0200

    Adaugare librari si partea de interactiune cu utilizatorul

---
+++ ProiectMAP1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numar;
+    
+    printf("\nIntroduceti numarul:");
+    scanf("%d", &numar);
+
+    printf("Numarul convertit in cifre romane: ");
+
+    return 0;
+}
